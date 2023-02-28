91667a31803ad3f3a4a5ee69475081bb42c810fe
FallforAsuka <fallfor.asuka@gmail.com>
2023-02-28 16:46:38 +0800

    冒泡排序

---
+++ My_BubbleSort.c
@@ -0,0 +1,17 @@
+/*冒泡排序bubble_sort*/
+void my_bubble_sort(int *num,int len)
+{
+	int i,j,tmp;
+	for(i=0;i<len-1;i++)
+	{
+		for(j=0;j<len-1-i;j++)
+		{
+			if(num[j]>num[j+1])
+			{
+				tmp=num[j];
+				num[j]=num[j+1];
+				num[j+1]=tmp;
+			}
+		}
+	}
+}
